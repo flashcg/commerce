74837b271027ca8ac098842d7633dbc63704489e
lzc <lzc@sy-ruifeng.com>
2021-05-23 00:20:37 +0800

    init vue pages

---
+++ pages/open-superconverter/open-superconverter-screenshot.vue
@@ -0,0 +1,18 @@
+<template>
+<nuxt-content class="py-6" :document="mddata" />
+</template>
+<script>
+export default {
+async asyncData({app,$content}){
+let  mddata = await $content('pages/open-superconverter/open-superconverter-screenshot').fetch();
+ mddata = await app.$initMD(mddata);
+ return {mddata}
+},
+head() {   
+ return this.mddata.head
+},
+mounted() {
+  window.$nuxt.setLayout(this.mddata.layout)
+}
+};
+</script>
