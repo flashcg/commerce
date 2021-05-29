a8776eaa317a8ab9f736314e5f8f8500b360b503
lzc <lzc@sy-ruifeng.com>
2021-05-29 12:06:03 +0800

    generated pages

---
+++ pages/dvd-to-ipod-converter/articles/what-is-the-best-way-to-do-a-disk-with-multiple-episodes copy.vue
@@ -0,0 +1,15 @@
+<template>
+<nuxt-content class="py-6" :document="mddata" />
+</template>
+<script>
+export default {
+async asyncData({app,$content}){
+let  mddata = await $content('pages/dvd-to-ipod-converter/articles/what-is-the-best-way-to-do-a-disk-with-multiple-episodes copy').fetch();
+ mddata = await app.$initMD(mddata);
+ return {mddata}
+},
+head() {   
+ return this.mddata.head
+}
+};
+</script>
